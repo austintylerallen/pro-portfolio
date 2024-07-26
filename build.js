b93b9284eed5896a10e470bcb6714a94be1a4879
austinallen <austintallen07@gmail.com>
2024-07-26 04:21:48 -0400

    Remove Babel and update build setup for esbuild

---
+++ build.js
@@ -0,0 +1,12 @@
+const { build } = require('esbuild');
+const { sassPlugin } = require('esbuild-sass-plugin');
+
+build({
+  entryPoints: ['src/index.js'],
+  bundle: true,
+  outdir: 'build',
+  minify: true,
+  sourcemap: true,
+  define: { 'process.env.NODE_ENV': '"production"' },
+  plugins: [sassPlugin()],
+}).catch(() => process.exit(1));
