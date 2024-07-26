232eb8186fc350e25bced954265126cc3703a6c3
austinallen <austintallen07@gmail.com>
2024-07-26 07:11:05 -0400

    Update esbuild configuration to handle JSX and assets

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
-console.log('Starting esbuild...');
 const { build } = require('esbuild');
 const { sassPlugin } = require('esbuild-sass-plugin');
+
+console.log('Starting esbuild...');
 
 build({
   entryPoints: ['src/index.js'],
@@ -9,5 +10,13 @@ build({
   minify: true,
   sourcemap: true,
   define: { 'process.env.NODE_ENV': '"production"' },
+  loader: {
+    '.js': 'jsx',
+    '.jsx': 'jsx',
+    '.svg': 'file',
+    '.jpg': 'file',
+    '.png': 'file',
+    '.mp4': 'file'
+  },
   plugins: [sassPlugin()],
 }).catch(() => process.exit(1));
